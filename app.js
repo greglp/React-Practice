0cfad2fff5d26af5b7c541af0d0c7fd767116d00
greg <greglp@users.noreply.github.com>
2022-07-15 21:51:09 -0700

    Setup basic components of react app

---
+++ app.js
@@ -0,0 +1,41 @@
+const Header = () =>
+    <header>
+        <h1>Scoreboard</h1>
+        <span className="stats">Players: 1</span>
+    </header>;
+
+const Player = () => {
+    return (
+        <div className="player">
+            <span className="player-name">
+                Daisy
+            </span>
+            <Counter />
+        </div>
+    );
+}
+
+const Counter = () => {
+    return (
+        <div className="counter">
+            <button className="counter-action decrement"> - </button>
+            <span className="counter-score">100</span>
+            <button className="counter-action increment"> + </button>
+        </div>
+    );
+}
+
+const App = () => {
+    return (
+        <div className="scoreboard">
+            <Header />
+            {/* Players list */}
+            <Player />
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
